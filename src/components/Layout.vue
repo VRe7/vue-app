79525c64ab347d66e21ab55e397eed9b51d97d05
vnaLc <1798268884@qq.com>
2019-03-02 10:11:19 +0800

    【Layout】母版页

---
+++ src/components/Layout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app_wrapper">
+    <!-- main -->
+    <div class="main_wrapper">
+      <h1 class="title">{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  /*layout*/
+  /*此时rem还没引入（Step3）*/
+  /*此时sass也还不能用（Step4）*/
+  .app_wrapper {
+    background-color: #f2f2f2;
+    .main_wrapper{
+      .title{
+        font-size: 1rem;
+        color: #f00
+      }
+    }
+  }
+  .main_wrapper .title {
+    font-size: 1rem;
+    color: #ff4222;
+  }
+</style>
